34e0809f9896e9979e3c8284986ebe0b991c770d
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-08-04 13:35:08 +0530

    project initialized

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <View>
+        <Text>{'hiiiii'}</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
